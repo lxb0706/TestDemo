71ccc4a584eea3a49c14e90eb69229cdbc8e78aa
Chris <lxb0706@gmail.com>
2017-11-25 18:35:58 +0800

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by Chris on 2017/11/25.
+//  Copyright © 2017年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
